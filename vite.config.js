7263d94ffe9b1409adbce281f747778f9a86a658
Dante <dante.dubon@gmail.com>
2021-06-14 17:34:27 -0600

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
+import { defineConfig } from "vite"
+import pugPlugin from "vite-plugin-pug"
 const { resolve } = require('path')
+
+const options = { pretty: true } // FIXME: pug pretty is deprecated!
+const locals = { name: "My Pug" }
+
 export default {
-    plugins: [],
+    plugins: [pugPlugin(options, locals)],
     server: {
         open: '/index.html',
         port: 3116,
